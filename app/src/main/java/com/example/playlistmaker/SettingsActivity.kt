496100168959263d94e2c67c80ed974027d7962f
yum3000 <fooco800@gmail.com>
2025-01-22 19:18:33 +0300

    Домашнее задание 1 (спринт 8)

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -3,9 +3,9 @@ package com.example.playlistmaker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 }
