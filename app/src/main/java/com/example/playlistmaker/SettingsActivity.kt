7991fc3645f65de260161b6cef2e5e7bead28c59
yum3000 <fooco800@gmail.com>
2025-01-28 15:51:45 +0300

    Домашняя работа 2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,14 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import android.widget.Toast
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -15,5 +21,67 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val shareTextView = findViewById<TextView>(R.id.share_textview)
+        val shareIcon = findViewById<ImageView>(R.id.share_icon)
+
+        val shareClickListener = View.OnClickListener {
+            shareApp()
+        }
+
+        shareTextView.setOnClickListener(shareClickListener)
+        shareIcon.setOnClickListener(shareClickListener)
+
+        val supportTextView = findViewById<TextView>(R.id.support_textview)
+        val supportIcon = findViewById<ImageView>(R.id.support_icon)
+
+        val supportClickListener = View.OnClickListener {
+            writeToSupport()
+        }
+
+        supportTextView.setOnClickListener(supportClickListener)
+        supportIcon.setOnClickListener(supportClickListener)
+
+        val userAgreeTextView = findViewById<TextView>(R.id.user_agreement_textview)
+        val userAgreeIcon = findViewById<ImageView>(R.id.user_agreement_icon)
+
+        val agreementClickListener = View.OnClickListener {
+            openUserAgreement()
+        }
+
+        userAgreeTextView.setOnClickListener(agreementClickListener)
+        userAgreeIcon.setOnClickListener(agreementClickListener)
+
+    }
+
+    private fun shareApp(){
+        val shareMsg = getString(R.string.share_app_msg)
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMsg)
+        startActivity(Intent.createChooser(shareIntent, null))
+    }
+
+    private fun writeToSupport() {
+        val email = getString(R.string.support_email)
+        val subject = getString(R.string.support_subject)
+        val body = getString(R.string.support_body)
+
+        val supportIntent = Intent(Intent.ACTION_SENDTO)
+        supportIntent.data = Uri.parse("mailto:")
+        supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        supportIntent.putExtra(Intent.EXTRA_TEXT, body)
+
+        if (supportIntent.resolveActivity(packageManager) != null) {
+            startActivity(supportIntent)
+        } else {
+            Toast.makeText(this, getString(R.string.no_email_clients), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openUserAgreement() {
+        val practicumOffer = Uri.parse(getString(R.string.practicum_offer))
+        val agreementIntent = Intent(Intent.ACTION_VIEW, practicumOffer)
+        startActivity(agreementIntent)
     }
 }
