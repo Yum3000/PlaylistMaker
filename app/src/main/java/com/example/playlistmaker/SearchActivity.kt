990d67ec0221218eea6e7f99fef1f37c04211c1d
yum3000 <fooco800@gmail.com>
2025-02-03 21:18:13 +0300

    Реализация мок-списка на экране Search

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.google.android.material.appbar.MaterialToolbar
 import android.view.inputmethod.InputMethodManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchActivity : AppCompatActivity() {
     private var inputedText: String = ""
@@ -25,7 +27,7 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
 
-        inputSearchLine = findViewById<EditText>(R.id.search_edit_text)
+        inputSearchLine = findViewById(R.id.search_edit_text)
 
         if (savedInstanceState != null) {
             inputedText = savedInstanceState.getString(SEARCH_TEXT, "")
@@ -65,6 +67,40 @@ class SearchActivity : AppCompatActivity() {
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.showSoftInput(inputSearchLine, InputMethodManager.SHOW_IMPLICIT)
         }
+
+        val trackList: MutableList<Track> = mutableListOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana", "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "Michael Jackson", "4:35",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "Bee Gees", "4:10",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "Led Zeppelin", "5:33",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "Guns N' Roses", "5:03",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        ))
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = TrackAdapter(trackList)
+        recyclerView.adapter = adapter
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -91,4 +127,5 @@ class SearchActivity : AppCompatActivity() {
         const val SEARCH_TEXT = "SEARCH_TEXT"
         const val SEARCH_FOCUS = "SEARCH_FOCUS"
     }
+
 }
